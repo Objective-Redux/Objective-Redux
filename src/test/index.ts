527308edc21584df9b2dcee4d7a266e5589ba715
Jason Mace <jmace01@gmail.com>
2021-04-01 23:45:18 -0700

    no longer exporting all files from the test index

--- src/test/index.ts
+++ src/test/index.ts
@@ -10,23 +10,5 @@
 
 /* istanbul ignore file */
 
-// Mocked
 export { StatelessControllerMock as StatelessController } from './stateless-controller-mock';
 export { StateControllerMock as StateController } from './state-controller-mock';
-
-// Real
-export { ObjectiveStore } from '../store/objective-store';
-export { ReducerInjector } from '../store/reducer-injector';
-export { createAction, getActionNameForController } from '../helpers/action';
-export { ObjectiveStoreProvider } from '../components/objective-store-provider';
-export { ComponentConnector } from '../components/component-connector';
-export { getObjectiveStoreFromSagaContext } from '../hooks/get-objective-store-from-saga-context';
-export { getControllerFromSagaContext } from '../hooks/get-controller-from-saga-context';
-export { useObjectiveStore } from '../hooks/use-objective-store';
-export { useController } from '../hooks/use-controller';
-export {
-  configureTakeLatest,
-  configureTakeEvery,
-  configureTakeLeading,
-  configureDebounce,
-} from '../helpers/effect-type';
