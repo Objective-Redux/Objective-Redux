bfd881c990b6ea2dfcff57cfa376dda5cd34c0f5
Jason Mace <jmace01@gmail.com>
2021-04-02 13:38:03 -0700

    Version 0.25.0

--- dist/test/index.js
+++ dist/test/index.js
@@ -9,34 +9,9 @@
 // the LICENSE file, found in the project's root directory.
 // ================================================================================================
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.StateController = exports.StatelessController = void 0;
 /* istanbul ignore file */
-// Mocked
 var stateless_controller_mock_1 = require("./stateless-controller-mock");
 Object.defineProperty(exports, "StatelessController", { enumerable: true, get: function () { return stateless_controller_mock_1.StatelessControllerMock; } });
 var state_controller_mock_1 = require("./state-controller-mock");
 Object.defineProperty(exports, "StateController", { enumerable: true, get: function () { return state_controller_mock_1.StateControllerMock; } });
-// Real
-var objective_store_1 = require("../store/objective-store");
-Object.defineProperty(exports, "ObjectiveStore", { enumerable: true, get: function () { return objective_store_1.ObjectiveStore; } });
-var reducer_injector_1 = require("../store/reducer-injector");
-Object.defineProperty(exports, "ReducerInjector", { enumerable: true, get: function () { return reducer_injector_1.ReducerInjector; } });
-var action_1 = require("../helpers/action");
-Object.defineProperty(exports, "createAction", { enumerable: true, get: function () { return action_1.createAction; } });
-Object.defineProperty(exports, "getActionNameForController", { enumerable: true, get: function () { return action_1.getActionNameForController; } });
-var objective_store_provider_1 = require("../components/objective-store-provider");
-Object.defineProperty(exports, "ObjectiveStoreProvider", { enumerable: true, get: function () { return objective_store_provider_1.ObjectiveStoreProvider; } });
-var component_connector_1 = require("../components/component-connector");
-Object.defineProperty(exports, "ComponentConnector", { enumerable: true, get: function () { return component_connector_1.ComponentConnector; } });
-var get_objective_store_from_saga_context_1 = require("../hooks/get-objective-store-from-saga-context");
-Object.defineProperty(exports, "getObjectiveStoreFromSagaContext", { enumerable: true, get: function () { return get_objective_store_from_saga_context_1.getObjectiveStoreFromSagaContext; } });
-var get_controller_from_saga_context_1 = require("../hooks/get-controller-from-saga-context");
-Object.defineProperty(exports, "getControllerFromSagaContext", { enumerable: true, get: function () { return get_controller_from_saga_context_1.getControllerFromSagaContext; } });
-var use_objective_store_1 = require("../hooks/use-objective-store");
-Object.defineProperty(exports, "useObjectiveStore", { enumerable: true, get: function () { return use_objective_store_1.useObjectiveStore; } });
-var use_controller_1 = require("../hooks/use-controller");
-Object.defineProperty(exports, "useController", { enumerable: true, get: function () { return use_controller_1.useController; } });
-var effect_type_1 = require("../helpers/effect-type");
-Object.defineProperty(exports, "configureTakeLatest", { enumerable: true, get: function () { return effect_type_1.configureTakeLatest; } });
-Object.defineProperty(exports, "configureTakeEvery", { enumerable: true, get: function () { return effect_type_1.configureTakeEvery; } });
-Object.defineProperty(exports, "configureTakeLeading", { enumerable: true, get: function () { return effect_type_1.configureTakeLeading; } });
-Object.defineProperty(exports, "configureDebounce", { enumerable: true, get: function () { return effect_type_1.configureDebounce; } });
