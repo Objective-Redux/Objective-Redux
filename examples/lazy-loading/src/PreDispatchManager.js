3bbb394f7c6e8ed2cbaef3e0306b346526a4bcf7
Jason Mace <jmace01@gmail.com>
2021-10-20 22:29:42 -0700

    Updated docs and examples

---
+++ examples/lazy-loading/src/PreDispatchManager.js
@@ -0,0 +1,88 @@
+// ================================================================================================
+//                                          OBJECTIVE REDUX
+//                                  Redux made better, objectively.
+//
+// (c) Copyright 2021 by Jason Mace (https://github.com/jmace01)
+//
+// This project is provided under the terms of the MIT license. The license details can be found in
+// the LICENSE file, found in the project's root directory.
+// ================================================================================================
+
+import React from 'react';
+
+/**
+ * A simple example implementation for a React.lazy pre-dispatch hook.
+ * This allows you to lazy-load React components along with their controllers
+ * when an action targeting the lazy-loaded bundle is fired.
+ *
+ * @example
+ * ```
+ * const preDispatchManager = new PreDispatchManager();
+ *
+ * const myActionName = getActionNameForController('MyControllerInBundle', 'action-name-on-controller');
+ * const LazyComponent = preDispatchManager.watchForActionWithComponent(
+ *   myActionType,
+ *   () => import('./lazy-module')
+ * );
+ *
+ * const objectiveStore = new ObjectiveStore({
+ *   preDispatchHook: preDispatchManager.loadComponentForAction.bind(preDispatchManager),
+ * });
+ * ```
+ *
+ * See the index.js file for more.
+ */
+export class PreDispatchManager {
+  importFns = {};
+
+  /**
+   * Adds a webpack module with a React component that needs to be lazily-loaded when
+   * an action with the value of actionType is fired.
+   *
+   * @param actionType The name of the action for which Objective-Redux will watch.
+   * @param importFn A function to import the webpack module.
+   * @returns A React.lazy component instance.
+   */
+  watchForActionWithComponent(actionType, importFn) {
+    // Add a new entry into our map of actions for which the ObjectiveStore will watch
+    this.importFns[actionType] = {};
+
+    // Create a React.lazy component that will resolve when the action is fired and the
+    // bundle has been initialized/setup
+    const lazy = React.lazy(() => new Promise(
+      rs => {
+        this.importFns[actionType].promise = rs;
+      }
+    ));
+
+    // Add the webpack import function
+    this.importFns[actionType].importFn = importFn;
+
+    // Return the React.lazy component so it can be used in the DOM
+    return lazy;
+  }
+
+  loadComponentForAction(action) {
+    // Make sure this is an action we care about
+    if (action && action.type && !this.importFns[action.type]) {
+      // Returning a non-promise tells the ObjectiveStore not to wait before
+      // firing the action.
+      return null;
+    }
+
+    // Get the previously saved date for the action
+    const {
+      importFn,
+      promise,
+    } = this.importFns[action.type];
+
+    // Import the webpack module
+    const importPromise = importFn().then(imported => promise(imported));
+
+    // Stop watching for the action-- no need to try importing more than once
+    delete this.importFns[action.type];
+
+    // Return the promise to let the ObjectiveStore know when we're finished
+    return importPromise;
+  }
+}
--- examples/lazy-loading/src/index.jsx
+++ examples/lazy-loading/src/index.jsx
@@ -16,37 +16,49 @@ import {
   createAction,
   getActionNameForController,
 } from 'objective-redux';
+import { PreDispatchManager } from './PreDispatchManager';
 
-const applicationRoot= document.createElement('div');
-applicationRoot.id = 'appMain';
-document.body.appendChild(applicationRoot);
+// Action in the lazy-loaded bundle we want to target
+//
+// Note that the, when the page loads, the controller for this action is not present, yet.
+// Objective-Redux will see that the bundle needs to be loaded before the action can be
+// fired and handle the loading and setup.
+const lazyLoadedComponentAction = getActionNameForController('lazy', 'test');
 
-const lazyLoadAction = getActionNameForController('lazy', 'test');
+// Setup the pre-dispatch hook using an example implementation.
+//
+// There are many ways you can setup the lazy-loading maps. The PreDispatchManager,
+// defined in the PreDispatchManage.js class of this example, shows one approach.
+const preDispatchManager = new PreDispatchManager();
 
-let resolve = null;
-const LazyComponent = React.lazy(() => new Promise(
-  rs => { resolve = rs; }
-));
+const LazyComponent = preDispatchManager.watchForActionWithComponent(
+  lazyLoadedComponentAction,
+  () => import(/* webpackChunkName: "lazy" */ './lazy-module')
+);
 
-const preDispatchHook = action => {
-  switch (action?.type) {
-    case lazyLoadAction:
-      return import(/* webpackChunkName: "lazy" */ './lazy-module').then(imported => resolve(imported));
-    default:
-      return null;
-  }
-};
-
+// Setup the ObjectiveStore
+//
+// We are connecting our pre-dispatch hook, which will allow Objective-Redux to do its
+// magic.
 const objectiveStore = new ObjectiveStore({
-  preDispatchHook,
+  preDispatchHook: preDispatchManager.loadComponentForAction.bind(preDispatchManager),
 });
 
-const action = createAction(lazyLoadAction);
-
+// Setup a function to fire our action in the lazy-loaded bundle
+//
+// This will fire an action that cannot be resolved until the lazy-loaded bundle has
+// been loaded and setup. When Objective-Redux sees this action fired, it will download
+// the bundle, setup the controllers, resolve the React.lazy component, and fire the
+// action.
+const action = createAction(lazyLoadedComponentAction);
 const load = () => {
   objectiveStore.dispatch(action());
 };
 
+// Output the page
+const applicationRoot = document.createElement('div');
+applicationRoot.id = 'appMain';
+document.body.appendChild(applicationRoot);
 ReactDOM.render(
   <ObjectiveStoreProvider objectiveStore={objectiveStore}>
     <Suspense fallback={<div>Click the button</div>}>
