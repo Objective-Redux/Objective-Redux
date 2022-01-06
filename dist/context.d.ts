4258930544310fce8f00c37e83a28304bd5c672c
Jason Mace <jmace01@gmail.com>
2022-01-05 21:04:17 -0800

    Removed dist folder from commits

--- dist/context.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { ObjectiveStore } from './store/objective-store';
-/**
- * @internal
- */
-export declare const ObjectiveStoreProviderContext: React.Context<ObjectiveStore | null>;
-//# sourceMappingURL=context.d.ts.map
--- dist/store/pre-dispatch-hook-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-import { PreDispatchHookFn } from './objective-store';
-/**
- * @internal
- */
-export declare function preDispatchHookMiddleware(preDispatchHook: PreDispatchHookFn): any;
-//# sourceMappingURL=pre-dispatch-hook-middleware.d.ts.map
