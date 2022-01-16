dc942d94816998015f1ef51eaebcb403844e8edb
Jason Mace <jmace01@gmail.com>
2022-01-15 17:22:32 -0800

    Updated copyright for 2022

--- tests/unit/test/hooks/hook-subscriber.test.ts
+++ tests/unit/test/hooks/hook-subscriber.test.ts
@@ -2,7 +2,7 @@
 //                                          OBJECTIVE REDUX
 //                                  Redux made better, objectively.
 //
-// (c) Copyright 2021 by Jason Mace (https://github.com/jmace01)
+// (c) Copyright 2022 by Jason Mace (https://github.com/jmace01)
 //
 // This project is provided under the terms of the MIT license. The license details can be found in
 // the LICENSE file, found in the project's root directory.
